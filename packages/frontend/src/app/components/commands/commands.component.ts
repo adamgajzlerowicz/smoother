aecdc47abed0c6463b79248061ba99e903b07596
adamgajzlerowicz <nelf86@gmail.com>
2018-10-27 21:12:16 +0200

    re

--- packages/frontend/src/app/components/commands/commands.component.ts
+++ packages/frontend/src/app/components/commands/commands.component.ts
@@ -1,13 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewContainerRef, ComponentFactoryResolver, ViewChild } from '@angular/core';
 
-import { Config } from '../../../../../types';
+import { GitCommandComponent } from './git-command/git-command.component';
+import { AnyCommandComponent } from './any-command/any-command.component';
+
+import { Command } from '../../../../../types';
 
 @Component({
   selector: 'app-commands',
-  templateUrl: './commands.component.html',
+  template: `<div #placeholder></div>`,
   styleUrls: ['./commands.component.scss']
 })
-export class CommandsComponent {
-  @Input() config: Config;
-  constructor() { }
-}
+export class CommandsComponent implements AfterViewInit {
+  @Input() command: Command;
+
+  @ViewChild("placeholder", {read: ViewContainerRef}) placeholderRef: ViewContainerRef;
+
+   constructor(
+     private componentFactoryResolver: ComponentFactoryResolver) {
+   }
+
+   ngAfterViewInit() {
+    switch(this.command.command) {
+      case "gitCommand":
+        this.loadItem(GitCommandComponent);
+      break;
+      case "anyCommand":
+        this.loadItem(AnyCommandComponent);
+    }
+  }
+
+  private loadItem(component) {
+   const factory = this.componentFactoryResolver.resolveComponentFactory(component);
+   const componentRef = this.placeholderRef.createComponent(factory);
+       // @ts-ignore
+       componentRef.instance.command = this.command;
+       componentRef.changeDetectorRef.detectChanges();
+     }
+   }
